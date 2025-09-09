7acf624514f5564e49eb338aa28b8f49dda7a846
Роман Зорин <sgena170315@gmail.com>
2025-09-09 17:07:04 +0300

    Add project files.

---
+++ Zorin_Roman_ISP-232_Lab10/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Zorin_Roman_ISP_232_Lab10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var monster = new
+            {
+                Name = "dragon",
+                Damage = 50,
+                IsBoss = true,
+                Description = "Древний огнедышаший змей"
+            };
+
+            Console.WriteLine(monster.Description);
+        }
+    }
+}
