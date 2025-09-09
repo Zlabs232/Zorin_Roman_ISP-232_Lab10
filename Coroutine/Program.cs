fee3750ad0058208b5417dc447678c35288e0b60
Роман Зорин <sgena170315@gmail.com>
2025-09-09 18:26:55 +0300

    был добавлен пример корутины

---
+++ Coroutine/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static IEnumerable<int> GenerateNumbers()
+    {
+        Console.WriteLine("Начинаем генерацию...");
+        yield return 1;
+        Console.WriteLine("Пауза...");
+        yield return 2;
+        Console.WriteLine("И ещё раз...");
+        yield return 3;
+        Console.WriteLine("Готово!");
+    }
+
+    static void Main(string[] args)
+    {
+        foreach (int number in GenerateNumbers())
+        {
+            Console.WriteLine($"Получено число: {number}");
+        }
+    }
+}
