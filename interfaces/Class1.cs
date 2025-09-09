fb0dbb336e7e82c60a81cc232684eb325e9134f6
Роман Зорин <sgena170315@gmail.com>
2025-09-09 17:24:37 +0300

    Был добавлен пример работы с интерфейсами классов

---
+++ interfaces/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace interfaces
+{
+    public interface ISoungMaker
+    {
+        void MakeSound();
+
+    }
+    public class Cat : ISoungMaker
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("МяУ!");
+        }
+    }
+    public class Phone : ISoungMaker
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Дзынь-дзынь!");
+        }
+    }
+}
---
+++ interfaces/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ISoungMaker> soundMakers = new List<ISoungMaker>();
+            foreach (var item in soundMakers)
+            {
+                item.MakeSound();
+            }
+        }
+    }
+}
