77efc8867e9474c7b03a864d85411c631f503f18
Роман Зорин <sgena170315@gmail.com>
2025-09-09 18:21:47 +0300

    Были добавлены два метода приготовления супа и салата

--- async/Program.cs
+++ async/Program.cs
@@ -13,20 +13,48 @@ namespace async
 
             await Task.WhenAll(boilTask, sliceTask);
             Console.WriteLine("3. Завариваем чай!");
+
+            Console.WriteLine("Начинаем готовить ужин...");
+            await CookDinnerAsync();
+            Console.WriteLine("Ужин готов!");
         }
-        
+
         static async Task BoilWaterAsync()
         {
             Console.WriteLine("2. Кипятим воду (ждём 3 секунды)...");
             await Task.Delay(3000);
             Console.WriteLine("... Вода закипела");
         }
-        
+
         static async Task SliceLemonAsync()
         {
             Console.WriteLine("3. Режем лимон (ждём 2 секунды)...");
             await Task.Delay(2000);
             Console.WriteLine("...лимон нарезан");
+        }
+
+        public static async Task<string> MakeSoupAsync()
+        {
+            Console.WriteLine("Начали варить суп...");
+            await Task.Delay(3000);
+            return "Суп";
+        }
+        public static async Task<string> MakeSaladAsync()
+        {
+            Console.WriteLine("Начали нарезать салат...");
+            await Task.Delay(1000);
+            return "Салат";
+        }
+
+        public static async Task CookDinnerAsync()
+        {
+            Task<string> soupTask = MakeSoupAsync();
+            Task<string> saladTask = MakeSaladAsync();
+
+            string soup = await soupTask;
+            string salad = await saladTask;
+
+            Console.WriteLine($"Готово: {soup} и {salad}");
         }
     }
 }
