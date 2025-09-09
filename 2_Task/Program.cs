5b8a78458215a8c1806e983ed13bbbb8724ac422
Роман Зорин <sgena170315@gmail.com>
2025-09-09 19:40:15 +0300

    Задание 2 — Лямбда-выражения

---
+++ 2_Task/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _2_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> digits = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+            List<int> evenD = digits.FindAll(n => n % 2 == 0);
+
+            List<int> res = digits.FindAll(n => n % 3 == 0);
+
+            Console.WriteLine("Чётные числа: " + string.Join(", ", evenD));
+            Console.WriteLine();
+
+            Console.WriteLine("Числа, кратные 3: " + string.Join(", ", res));
+        }
+    }
+}
