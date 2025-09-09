b47a3f7780a82936121ab10272bc3c7ab65ea716
Роман Зорин <sgena170315@gmail.com>
2025-09-09 20:03:35 +0300

    Задание 4 — LINQ-запросы

---
+++ 4_Task/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _4_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string> { "Волгоград", "Киров", "Питер", "Дубай", "АбуДаби", "Краснодар", "Красноярск"};
+
+            var CSWK = list.Where(city => city.StartsWith("К"));
+            Console.WriteLine("Города на 'К': " + string.Join(", ", CSWK));
+
+            var SBL = list.OrderBy(city => city.Length);
+            Console.WriteLine("Length: " + string.Join(", ", SBL));
+
+            var LC = list.Where(city => city.Length > 6);
+            Console.WriteLine("> 6: " + string.Join(", ", LC));
+        }
+    }
+}
