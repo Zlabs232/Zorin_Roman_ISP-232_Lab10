6654999a517c77f551e92706c96b540c625bfa0f
Роман Зорин <sgena170315@gmail.com>
2025-09-09 18:38:42 +0300

    Были добавлены 2 примера async и corountine

--- Coroutine/Program.cs
+++ Coroutine/Program.cs
@@ -1,21 +1,53 @@
-﻿class Program
+﻿//class Program
+//{
+//    static IEnumerable<int> GenerateNumbers()
+//    {
+//        Console.WriteLine("Начинаем генерацию...");
+//        yield return 1;
+//        Console.WriteLine("Пауза...");
+//        yield return 2;
+//        Console.WriteLine("И ещё раз...");
+//        yield return 3;
+//        Console.WriteLine("Готово!");
+//    }
+
+//    static void Main(string[] args)
+//    {
+//        foreach (int number in GenerateNumbers())
+//        {
+//            Console.WriteLine($"Получено число: {number}");
+//        }
+//    }
+//}
+
+using System.Threading.Tasks;
+
+class Program
 {
-    static IEnumerable<int> GenerateNumbers()
+    static async Task Main()
     {
-        Console.WriteLine("Начинаем генерацию...");
-        yield return 1;
-        Console.WriteLine("Пауза...");
-        yield return 2;
-        Console.WriteLine("И ещё раз...");
-        yield return 3;
-        Console.WriteLine("Готово!");
+        foreach (var step in WaitCoroutine())
+        {
+            Console.WriteLine(step);
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine("Готово (корутина)");
+
+        await WaitAsync();
+        Console.WriteLine("Готово (асинхронно)");
+    }
+    static IEnumerable<string> WaitCoroutine()
+    {
+        yield return "Ждём... 1 секунда";
+        yield return "Ждём... 2 секунда";
     }
 
-    static void Main(string[] args)
+    static async Task WaitAsync()
     {
-        foreach (int number in GenerateNumbers())
-        {
-            Console.WriteLine($"Получено число: {number}");
-        }
+        Console.WriteLine("Асинхронное ожидание... 1 секунда");
+        await Task.Delay(1000);
+
+        Console.WriteLine("Асинхронное ожидание... 2 секунда");
+        await Task.Delay(1000);
     }
 }
